64cd5a358587e7e805ef54d7e27d0a5bf9f00658
Ben Matare <ben.a.matare@gmail.com>
2018-01-19 00:47:42 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,9 +13,8 @@ def loop_iterator(number_of_times)
 end
 
 def times_iterator(number_of_times)
-
   phrase = "Welcome to Flatiron School's Web Development Course!"
-
+puts phrase.times(number_of_times)
 end
 
 def while_iterator(number_of_times)
