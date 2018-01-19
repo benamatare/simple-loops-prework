163fc35c737038232be558f983f66899b3a9e2ae
Ben Matare <ben.a.matare@gmail.com>
2018-01-19 00:39:52 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -34,7 +34,7 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  for number_of_times in 1..100 do
+  for number_of_times in 1..7 do
     puts phrase
   end
   # code your solution here using the "for" keyword
